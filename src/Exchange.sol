2c41cbc3f5307e5c60f40609db668bb826f62f43
notchuks <chukaezem@gmail.com>
2024-09-09 17:16:51 +0100

    initial commit

---
+++ src/Exchange.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Exchange {
+    address public tokenAddress;
+
+    constructor(address _token) {
+        require(_token != address(0), "invalid token address");
+
+        tokenAddress = _token;
+    }
+
+    function addLiquidity(uint256 _tokenAmount) public payable {
+        IERC20 token = IERC20(tokenAddress);
+        token.transferFrom(msg.sender, address(this), _tokenAmount);
+    }
+
+    function getReserve() public view returns (uint256) {
+        return IERC20(tokenAddress).balanceOf(address(this));
+    }
+
+    function getPrice(uint256 inputReserve, uint256 outputReserve) public pure returns (uint256) {
+        require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
+        // increase precision cause solidity supports integer division with only floor rounding. 
+        return (inputReserve * 1000) / outputReserve;
+    }
+
+}
